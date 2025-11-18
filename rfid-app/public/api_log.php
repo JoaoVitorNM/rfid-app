b11aafd7faffa84200287486c2a001a90ee9ebb7
JoaoVitorNM <147427124+JoaoVitorNM@users.noreply.github.com>
2025-11-18 17:34:56 -0300

    Update api_log.php

--- rfid-app/public/api_log.php
+++ rfid-app/public/api_log.php
@@ -1,26 +1,28 @@
 <?php
 include("../config/db.php");
 
-$uid = $_GET['uid'] ?? null;
-$loc = $_GET['location'] ?? "Desconhecido";
+$rfid_uid = $_GET['uid'] ?? null;
+$location = $_GET['location'] ?? "Desconhecido";
 
-if (!$uid) die("UID ausente.");
+if (!$rfid_uid) die("UID não informado!");
 
-// Procura UID
-$stmt = $conn_tags->prepare("SELECT tag_id FROM tags WHERE rfid_uid=?");
-$stmt->bind_param("s", $uid);
+$sql = "SELECT tag_id FROM tags WHERE rfid_uid=?";
+$stmt = $conn_tags->prepare($sql);
+$stmt->bind_param("s", $rfid_uid);
 $stmt->execute();
 $res = $stmt->get_result();
 
-if ($res->num_rows == 0) {
-    die("UID não registrado!");
+if ($res->num_rows > 0) {
+    $tag = $res->fetch_assoc();
+    $tag_id = $tag['tag_id'];
+
+    $sql2 = "INSERT INTO access_logs (tag_id, reader_location) VALUES (?, ?)";
+    $stmt2 = $conn_tags->prepare($sql2);
+    $stmt2->bind_param("is", $tag_id, $location);
+    $stmt2->execute();
+
+    echo "Acesso registrado!";
+} else {
+    echo "Etiqueta não cadastrada!";
 }
 
-$tag = $res->fetch_assoc();
-
-// Insere log
-$stmt2 = $conn_tags->prepare("INSERT INTO access_logs (tag_id, reader_location) VALUES (?, ?)");
-$stmt2->bind_param("is", $tag['tag_id'], $loc);
-$stmt2->execute();
-
-echo "Acesso registrado!";
